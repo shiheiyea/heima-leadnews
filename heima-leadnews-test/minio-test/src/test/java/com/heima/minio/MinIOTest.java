7c58fae1eba044ba03bfa8c007f4b3a10014b72d
heiye <2759688992@qq.com>
2024-06-25 08:42:50 +0800

    MinIOTest同时导入MinIostarts

---
+++ heima-leadnews-test/minio-test/src/test/java/com/heima/minio/MinIOTest.java
@@ -0,0 +1,37 @@
+package com.heima.minio;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+
+import java.io.FileInputStream;
+
+public class MinIOTest {
+    /**
+     * 把list.html 文件上传到minio中，并且可以在浏览器中访问
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("G:\\list.html");
+
+            // 1.获取minio的链接信息 创建一个minio的客户端
+            MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://192.168.73.128:9000").build();
+
+            // 2.上传
+
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+                    .object("list.html") //文件名称
+                    .contentType("text/html") //文件类型
+                    .bucket("leadnews") // 桶名称 与minio管理界面创建的桶一致即可
+                    .stream(fileInputStream, fileInputStream.available(), -1)
+                    .build();
+
+            minioClient.putObject(putObjectArgs);
+
+            // 访问路径
+            System.out.println("http://192.168.73.128:9000/leadnews/list.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
