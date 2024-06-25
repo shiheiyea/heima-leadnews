05a9b75b87b49258fdbee6b111e1758b04dbdf16
heiye <2759688992@qq.com>
2024-06-25 11:48:23 +0800

    配置了自媒体模块的拦截器

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/WemediaApplication.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/WemediaApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @MapperScan("com.heima.wemedia.mapper")
 public class WemediaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WemediaApplication.class,args);
     }
