7b87915331c88e738e5483333c04c6182d5e0acb
Nite <2759688992@qq.com>
2024-07-11 17:33:13 +0800

    完成了redis延迟任务服务实现

---
+++ heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/service/TaskService.java
@@ -0,0 +1,12 @@
+package com.heima.schedule.service;
+
+import com.heima.model.schedule.dtos.Task;
+
+public interface TaskService {
+    /**
+     * 添加延迟任务
+     * @param task
+     * @return
+     */
+    public long addTask(Task task);
+}
